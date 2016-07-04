c4c8c066693ba92565393c836cbe2a786d95833c
Hillary Manning <hillary.b.manning@gmail.com>
2016-07-04 12:35:38 -0500

    fix chunking, add alert if no group number is chosen

--- lastOne.js
+++ lastOne.js
@@ -1,4 +1,4 @@
-var button;
+var button = null;
 var generatedGroups = [];
 var numArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
 var nameArr = [];
@@ -6,33 +6,29 @@ var nameArr = [];
 
 $(document).ready(function() {
 
-//button is temporarily hard-code: will be replaced by the value of the pressed button
-
-
 
 $(".buttons").on("click", "button", function () {
   button = parseInt($(this).attr("id"));
-  generatedGroups = generateGroups();
-
-  // console.log(button);
-  console.log(generatedGroups);
-
 });
 
 $(".generate").on("click","button", function () {
-  //  console.log(generatedGroups);
+  generatedGroups = generateGroups();
+   console.log(button);
+    if (button === null){
+      alert("Please select how many groups you need.");
+    }else{
     $(".groupsClass").empty();
     for(var i =0; i < generatedGroups.length; i++){
         var tempGroup = "group" + i;
-            $(".groupsClass").append("<div id=\""+tempGroup+"\"><ul>" + "Group"+(i + 1) + "</ul></div>");
-            console.log(tempGroup);
+        $(".groupsClass").append("<div id=\""+tempGroup+"\"><ul>" + "Group"+(i + 1) + "</ul></div>")
+        
+
+        console.log(tempGroup);
         for(var j =0; j < generatedGroups[i].length; j++){
             $("#"+tempGroup).append("<li>" + generatedGroups[i][j] + "</li>");
-          }
-}
-
-
-
+        }
+      }
+    }
 });
 
 //integer randomization is now possible
@@ -47,7 +43,7 @@ function generateGroups() {
   numArr = shuffle(numArr);
   //assigns associated names to the randomized numbers, creates an array of names in random order
   getName(numArr,nameArr);
-  var chunk = nameArr.length/button;
+  var chunk = (nameArr.length+1)/button;
   console.log(chunk);
 
   //creates an array of array: the nameArr order has already been randomized, so this breaks it into section of 'chunk' number of names.
@@ -58,7 +54,7 @@ function generateGroups() {
 
 }
 
-//SHUFFLE.
+//SHUFFLE
 function shuffle(array) {
   var m = array.length, t, i;
   // While there remain elements to shuffle…
@@ -77,7 +73,7 @@ function shuffle(array) {
 var createGroupedArray = function(arr, chunkSize) {
     var groups = [], i;
     for (i = 0; i < arr.length; i += chunkSize) {
-        groups.push(arr.slice(i, i + chunkSize));
+      groups.push(arr.slice(i, i + chunkSize));
     }
     return groups;
 }
@@ -151,7 +147,5 @@ function getName(arr, nameArr){
       }
     }
   };
-
-//DO THE THING
 
 });
